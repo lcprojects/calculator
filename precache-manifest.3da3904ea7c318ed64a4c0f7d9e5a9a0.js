e950ba40141ee383c77ddaff929f04e4a1d40ef5
lcprojects <43647132+lcprojects@users.noreply.github.com>
2020-02-27 20:27:36 +0000

    Updates

---
+++ precache-manifest.3da3904ea7c318ed64a4c0f7d9e5a9a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adeac0189851554a4d278175f02b643",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7f2f125393e88dcf1aae",
+    "url": "/calculator/static/css/main.8f193d8b.chunk.css"
+  },
+  {
+    "revision": "d6dba6d9e00332193034",
+    "url": "/calculator/static/js/2.3b449efd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calculator/static/js/2.3b449efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2f125393e88dcf1aae",
+    "url": "/calculator/static/js/main.3d1c1c80.chunk.js"
+  },
+  {
+    "revision": "ccfc56ce1b0c91cb76b8",
+    "url": "/calculator/static/js/runtime-main.627f4c10.js"
+  }
+]);
